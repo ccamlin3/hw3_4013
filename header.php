7916bb4f8c53718b73e32535c100bfdfc1a83421
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-10-04 23:48:58 -0500

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
-</html>
+  <body>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -39,3 +39,5 @@
     </div>
   </div>
 </nav>
+  </body>
+</html>
