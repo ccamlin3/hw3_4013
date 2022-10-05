eac8a52c3600b5189f818045e86895e057bf8547
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-10-05 14:10:31 -0500

    Update course-section.php

--- course-section.php
+++ course-section.php
@@ -22,7 +22,7 @@
     <?php
 
 $servername = "localhost";
-$username = "camcamli_homework3";
+$username = "camcamli_user1";
 $password = "gWLd!^-Ib?gJ";
 $dbname = "camcamli_homework3";
 // Create connection
