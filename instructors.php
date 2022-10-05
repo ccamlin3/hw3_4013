a3e12f68a54a10db00612c886f032c65e329f75e
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-10-05 00:09:10 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -1,22 +1,18 @@
+<?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Instructors</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Instructors</h1>
-<table class="table table-striped">
+
+  <table class="table table-striped">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Name</th>
+      <th>Instructor ID</th>
+      <th>Instructor Name</th>
+      <th>Course ID</th>
+      <th>Section ID</th>
     </tr>
   </thead>
   <tbody>
-    <?php
+<?php
 $servername = "localhost";
 $username = "camcamli_homework3";
 $password = "gWLd!^-Ib?gJ";
@@ -30,23 +26,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT instructor_id, instructor_name from instructor";
+$sql = "SELECT instructor_id, instructor_name, courseID, sectionID FROM instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["instructor_id"]?></td>
-    <td><a href="instructor-section.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
-  </tr>
-<?php
-  }
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+
+        echo '<tr>              
+                  <td scope="row">' . $row["instructor_id"]. '</td>
+                  <td>' . $row["instructor_name"] .'</td>
+                  <td>' . $row["courseID"] .'</td>
+                  <td>' . $row["sectionID"] .'</td>
+                   <br>                
+                </tr>';
+    }
 } else {
-  echo "0 results";
-}
-$conn->close();
+    echo "0 results";
+} 
 ?>
   </tbody>
     </table>
