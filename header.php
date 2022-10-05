767a13ee3a2df2083820344388f739e2b6968aaa
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-10-04 23:55:59 -0500

    Update header.php

--- header.php
+++ header.php
@@ -12,14 +12,14 @@
   <body>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Courses</a>
+          <a class="nav-link" href="index2.php">Courses</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="instructor.php">Instructors</a>
