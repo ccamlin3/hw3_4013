2cf9cb01b9956864f424554bc4d322b0cd82e067
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-10-04 23:46:18 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="instructor.php">Instructors</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="section.php">Sections</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="student.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="post.php">Submit</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</nav>
